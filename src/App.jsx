fcde313f4add67e9a5b6e502676cdb4a5bfa2b5f
roymansoor <roy.mansoor@gmail.com>
2024-02-11 15:22:02 -0600

    coming soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import LotakLogo from '/public/assets/LotakLogo.png'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const logoStyle = {
+    width: '50%',  // Set the width to be twice the original size
+    height: 'auto', // Maintain the aspect ratio
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        
+      <img src={LotakLogo} className="logo" alt="Lotak Realty Logo" style={logoStyle} />
+      
       </div>
       <h1>Lotak Realty</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+   
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Coming Soon
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
